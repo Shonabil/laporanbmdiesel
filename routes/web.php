7b0c1b784bcb64bd7269891ac0f3551f8a12773f
muhsyafwannabil <shonabil27@gmail.com>
2025-09-29 11:36:33 +0800

    pembaruan biar ga 404

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ use App\Http\Controllers\ProgressController;
 use App\Http\Controllers\ReportSectionController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
 /*
 |--------------------------------------------------------------------------
 | Auth Routes
@@ -59,7 +63,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
     });
 
     Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
-    
+
 
         // Report
         Route::get('/report', [ReportController::class, 'index'])->name('report.index');
